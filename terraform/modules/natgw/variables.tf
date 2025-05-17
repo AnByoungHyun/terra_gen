b8dce60eadcf0cc098dddff92f024d1558c2327f
AnByoungHyun <abhyuni@gmail.com>
2025-05-17 17:35:18 +0900

    fix: natgw 모듈에 vpc_id 변수 추가

--- terraform/modules/natgw/variables.tf
+++ terraform/modules/natgw/variables.tf
@@ -1,1 +1,2 @@
-variable "public_subnet_id" { description = "퍼블릭 서브넷 ID" } 
+variable "public_subnet_id" { description = "퍼블릭 서브넷 ID" }
+variable "vpc_id" { description = "VPC ID for NAT Gateway" } 
