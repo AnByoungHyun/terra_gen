8f776be2ee9c7d1783bc0ea566327f519aec0924
AnByoungHyun <abhyuni@gmail.com>
2025-05-18 18:06:07 +0900

    fix: Terraform 변수 선언 멀티라인 블록으로 수정(EKS Node Group)

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -2,9 +2,25 @@ variable "vpc_id" { description = "VPC ID" }
 variable "cluster_name" { description = "EKS 클러스터 이름" }
 variable "eks_role_arn" { description = "EKS 클러스터에 할당할 IAM Role ARN" }
 variable "subnet_ids" { description = "EKS 클러스터에 연결할 서브넷 ID 목록" }
-variable "node_group_name" { description = "EKS Node Group 이름" }
-variable "node_role_arn" { description = "EKS Node Group에 할당할 IAM Role ARN" }
-variable "node_instance_type" { description = "EKS 워커 노드 인스턴스 타입" default = "t3.medium" }
-variable "node_desired_size" { description = "EKS Node Group의 기본 노드 수" default = 2 }
-variable "node_max_size" { description = "EKS Node Group의 최대 노드 수" default = 3 }
-variable "node_min_size" { description = "EKS Node Group의 최소 노드 수" default = 1 } 
+variable "node_group_name" {
+  description = "EKS Node Group 이름"
+}
+variable "node_role_arn" {
+  description = "EKS Node Group에 할당할 IAM Role ARN"
+}
+variable "node_instance_type" {
+  description = "EKS 워커 노드 인스턴스 타입"
+  default     = "t3.medium"
+}
+variable "node_desired_size" {
+  description = "EKS Node Group의 기본 노드 수"
+  default     = 2
+}
+variable "node_max_size" {
+  description = "EKS Node Group의 최대 노드 수"
+  default     = 3
+}
+variable "node_min_size" {
+  description = "EKS Node Group의 최소 노드 수"
+  default     = 1
+} 
